196d196b06f19a204fbb71ac8e2604628f600737
yanmiao99 <503084944@qq.com>
2023-01-15 20:23:51 +0800

    feat : 处理跨域

--- management-system/src/setupProxy.js
+++ management-system/src/setupProxy.js
@@ -1,22 +1,19 @@
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        // 本地接口
-        createProxyMiddleware('/api', {   // 匹配所有带'/api'的接口地址进行代理转发
-            target: 'http://localhost:3221',  // 代理地址
+        '/v1/react-job/api/v1/',
+        createProxyMiddleware({
+            target: 'http://localhost:3221',
             changeOrigin: true,
-            pathRewrite: {
-                '^/api': ''  // 将请求地址中的'/api'重写为空再请求接口（也就是去掉/api）
-            }
+            // pathRewrite:{
+            //     '^/v1/react-job/api/v1/':''
+            // }
         }),
-        // 第三方接口
-        // createProxyMiddleware('/v.api.aa1.cn', {   // 匹配所有带'/api'的接口地址进行代理转发
-        //     target: 'https://v.api.aa1.cn/api',  // 代理地址
-        //     changeOrigin: true,
-        //     // pathRewrite: {
-        //     //     '^/third_party_api': ''  // 将请求地址中的'/api'重写为空再请求接口（也就是去掉/api）
-        //     // }
-        // })
-    )
-}
+        'v.api.aa1.cn',
+        createProxyMiddleware({
+            target: 'https://v.api.aa1.cn',
+            changeOrigin: true,
+        })
+    );
+};
