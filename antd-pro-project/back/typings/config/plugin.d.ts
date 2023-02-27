872da49add3d417caeb76745553e2aa53c19f220
yanmiao99 <503084944@qq.com>
2023-02-27 17:28:58 +0800

    feat : 重置后端

---
+++ antd-pro-project/back/typings/config/plugin.d.ts
@@ -0,0 +1,44 @@
+// This file is created by egg-ts-helper@1.34.7
+// Do not modify this file!!!!!!!!!
+/* eslint-disable */
+
+import 'egg';
+import 'egg-onerror';
+import 'egg-session';
+import 'egg-i18n';
+import 'egg-watcher';
+import 'egg-multipart';
+import 'egg-security';
+import 'egg-development';
+import 'egg-logrotator';
+import 'egg-schedule';
+import 'egg-static';
+import 'egg-jsonp';
+import 'egg-view';
+import 'egg-sequelize-ts';
+import 'egg-validate';
+import 'egg-session-redis';
+import 'egg-redis';
+import 'egg-cors';
+import { EggPluginItem } from 'egg';
+declare module 'egg' {
+  interface EggPlugin {
+    onerror?: EggPluginItem;
+    session?: EggPluginItem;
+    i18n?: EggPluginItem;
+    watcher?: EggPluginItem;
+    multipart?: EggPluginItem;
+    security?: EggPluginItem;
+    development?: EggPluginItem;
+    logrotator?: EggPluginItem;
+    schedule?: EggPluginItem;
+    static?: EggPluginItem;
+    jsonp?: EggPluginItem;
+    view?: EggPluginItem;
+    sequelize?: EggPluginItem;
+    validate?: EggPluginItem;
+    sessionRedis?: EggPluginItem;
+    redis?: EggPluginItem;
+    cors?: EggPluginItem;
+  }
+}
